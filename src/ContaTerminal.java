352fbdac587097eecd45976ec574ee30e8804f1a
vanessaneves-dev <vanessaneves.dev@gmail.com>
2023-09-28 19:11:03 -0300

    :tada: Commit inicial

---
+++ src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        // TODO: Conhecer e importar a class Scanner
+
+        // Exibir as mensagens para o usuário
+
+        // Obter os valores digitados no terminal
+
+        // Exibir a mensagem final
+    }
+}
